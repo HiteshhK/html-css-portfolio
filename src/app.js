267afd0b7ce77c367b3f626bced99dfbfce9c267
HiteshhK <kaushikhitesh002@gmail.com>
2022-02-19 12:46:27 +0530

    3 files modified

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log('hello!')
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+function toggleButton() {
+  navList.classList.toggle('show');
+}
+
+hamburgerButton.addEventListener('click', toggleButton);
